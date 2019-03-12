93515bc45de41d953504777dbc33183c449eabf9
JaredSalvatore <48420128+JaredSalvatore@users.noreply.github.com>
2019-03-12 13:33:59 -0400

    Initial commit

---
+++ Test.py
@@ -0,0 +1,10 @@
+#Yo.
+# Welcome to area calc.
+from tkinter import
+def calculate_area():
+    length = input("enter the length: ")
+    width = input("enter the width: ")
+    result = float(length) * float(width)
+    return result
+print (f'the area is: {calculate_area()}') 
+
